29f789ea00f334699ec5790049a3d9bb4a040a84
Joao Guilherme dos Santos <--global>
2025-09-08 07:11:17 -0400

    Update and delete routes created

--- api/api.go
+++ api/api.go
@@ -27,6 +27,9 @@ func NewHandler(db store.UserRepo) http.Handler {
 
 	r.Get("/users", getUsers(db))
 	r.Get("/users/{id}", getUserById(db))
+	r.Post("/users", addUser(db))
+	r.Patch("/users/{id}", update(db))
+	r.Delete("/users/{id}", deleteUser(db))
 
 	return r
 }
@@ -56,7 +59,7 @@ func getUsers(db store.UserRepo) http.HandlerFunc {
 	}
 }
 
-func AddUser(db store.UserRepo) http.HandlerFunc {
+func addUser(db store.UserRepo) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var body UserRequest
 
@@ -92,6 +95,73 @@ func AddUser(db store.UserRepo) http.HandlerFunc {
 		}
 
 		utils.JsonResponse(w, http.StatusCreated, map[string]any{
+			"user": user,
+		})
+	}
+}
+
+func update(db store.UserRepo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		var body UserRequest
+
+		err := json.NewDecoder(r.Body).Decode(&body)
+
+		if err != nil {
+			utils.JsonResponse(w, http.StatusUnprocessableEntity, map[string]any{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if utils.NotBlank(body.FirstName) {
+			body.Validator.CheckField(utils.MinChars(body.FirstName, 2) && utils.MaxChars(body.FirstName, 20), "first_name", "This field must have between 2 and 20 characters")
+		}
+		if utils.NotBlank(body.LastName) {
+			body.Validator.CheckField(utils.MinChars(body.LastName, 2) && utils.MaxChars(body.LastName, 20), "last_name", "This field must have between 2 and 20 characters")
+		}
+		if utils.NotBlank(body.Bio) {
+			body.Validator.CheckField(utils.MinChars(body.Bio, 20) && utils.MaxChars(body.Bio, 450), "bio", "This field must have between 20 and 450 characters")
+		}
+
+		if !body.Validator.Valid() {
+			utils.JsonResponse(w, http.StatusBadRequest, body.Validator.FieldErrors)
+			return
+		}
+
+		user, err := db.Update(id, store.User{
+			FirstName: body.FirstName,
+			LastName:  body.LastName,
+			Bio:       body.Bio,
+		})
+
+		if err != nil {
+			utils.JsonResponse(w, http.StatusNotFound, map[string]any{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		utils.JsonResponse(w, http.StatusOK, map[string]any{
+			"user": user,
+		})
+	}
+}
+
+func deleteUser(db store.UserRepo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+
+		user, err := db.Delete(id)
+
+		if err != nil {
+			utils.JsonResponse(w, http.StatusOK, map[string]any{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		utils.JsonResponse(w, http.StatusOK, map[string]any{
 			"user": user,
 		})
 	}
